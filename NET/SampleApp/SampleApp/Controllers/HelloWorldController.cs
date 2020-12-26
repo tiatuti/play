62a88bb5506eee524fcc21d1f2fe6b054b75ac28
SandraR <sandra_romero@ultimatesoftware.com>
2020-12-26 18:52:23 -0500

    cleanup

--- NET/SampleApp/SampleApp/Controllers/HelloWorldController.cs
+++ NET/SampleApp/SampleApp/Controllers/HelloWorldController.cs
@@ -6,6 +6,11 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
+using System.IO;
+using System.IO.Packaging;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+
 namespace SampleApp.Controllers
 {
     public class HelloWorldController : Controller
@@ -23,10 +28,22 @@ namespace SampleApp.Controllers
 
         public ActionResult Welcome()
         {
-            
-    
+
+            OpenSpreadsheetDocumentReadonly(@"C:\_Sandra\NET\SampleApp\WorkflowReport.16k.xlsx");
+
+            const string fileName =
+                @"C:\_Sandra\NET\SampleApp\WorkflowReport.16k.xlsx";
+
+            // Retrieve the value in cell A1.
+           // string value = GetCellValue(fileName, "Sheet1", "A2");
+           // System.Diagnostics.Debug.WriteLine(value);
+
+          //  ReadExcelFileSAX(fileName);    // SAX
+
+            ReadExcelFileDOM(fileName);
+
             var html = new HtmlDocument();
-            html.Load(@"C:\_Sandra\NET\SampleApp\HRSD.html"); // load from a file
+            //html.Load(@"C:\_Sandra\NET\SampleApp\HRSD.html"); // load from a file
 
             // load from a URL
             // html.LoadHtml(new WebClient().DownloadString("http://www.mikesdotnetting.com/article/273/using-the-htmlagilitypack-to-parse-html-in-asp-net"));
@@ -39,6 +56,165 @@ namespace SampleApp.Controllers
             ViewBag.Message = node.InnerHtml;
 
             return View();
+        }
+
+        // Retrieve the value of a cell, given a file name, sheet name, 
+        // and address name.
+        public static string GetCellValue(string fileName,
+            string sheetName,
+            string addressName)
+        {
+            string value = null;
+
+            // Open the spreadsheet document for read-only access.
+            using (SpreadsheetDocument document =
+                SpreadsheetDocument.Open(fileName, false))
+            {
+                // Retrieve a reference to the workbook part.
+                WorkbookPart wbPart = document.WorkbookPart;
+
+                // Find the sheet with the supplied name, and then use that 
+                // Sheet object to retrieve a reference to the first worksheet.
+                Sheet theSheet = wbPart.Workbook.Descendants<Sheet>().
+                  Where(s => s.Name == sheetName).FirstOrDefault();
+
+                // Throw an exception if there is no sheet.
+                if (theSheet == null)
+                {
+                    throw new ArgumentException("sheetName");
+                }
+
+                // Retrieve a reference to the worksheet part.
+                WorksheetPart wsPart =
+                    (WorksheetPart)(wbPart.GetPartById(theSheet.Id));
+
+                // Use its Worksheet property to get a reference to the cell 
+                // whose address matches the address you supplied.
+                Cell theCell = wsPart.Worksheet.Descendants<Cell>().
+                  Where(c => c.CellReference == addressName).FirstOrDefault();
+
+                // If the cell does not exist, return an empty string.
+                if (theCell.InnerText.Length > 0)
+                {
+                    value = theCell.InnerText;
+
+                    // If the cell represents an integer number, you are done. 
+                    // For dates, this code returns the serialized value that 
+                    // represents the date. The code handles strings and 
+                    // Booleans individually. For shared strings, the code 
+                    // looks up the corresponding value in the shared string 
+                    // table. For Booleans, the code converts the value into 
+                    // the words TRUE or FALSE.
+                    if (theCell.DataType != null)
+                    {
+                        switch (theCell.DataType.Value)
+                        {
+                            case CellValues.SharedString:
+
+                                // For shared strings, look up the value in the
+                                // shared strings table.
+                                var stringTable =
+                                    wbPart.GetPartsOfType<SharedStringTablePart>()
+                                    .FirstOrDefault();
+
+                                // If the shared string table is missing, something 
+                                // is wrong. Return the index that is in
+                                // the cell. Otherwise, look up the correct text in 
+                                // the table.
+                                if (stringTable != null)
+                                {
+                                    value =
+                                        stringTable.SharedStringTable
+                                        .ElementAt(int.Parse(value)).InnerText;
+                                }
+                                break;
+
+                            case CellValues.Boolean:
+                                switch (value)
+                                {
+                                    case "0":
+                                        value = "FALSE";
+                                        break;
+                                    default:
+                                        value = "TRUE";
+                                        break;
+                                }
+                                break;
+                        }
+                    }
+                }
+            }
+            return value;
+        }
+        private void OpenSpreadsheetDocumentReadonly(string filepath)
+        {
+            // Open a SpreadsheetDocument based on a filepath.
+            using (SpreadsheetDocument spreadsheetDocument = SpreadsheetDocument.Open(filepath, false))
+            {
+                WorkbookPart workbookPart = spreadsheetDocument.WorkbookPart;
+                WorksheetPart worksheetPart = workbookPart.WorksheetParts.First();
+                SheetData sheetData = worksheetPart.Worksheet.Elements<SheetData>().First();
+                string text;
+                foreach (Row r in sheetData.Elements<Row>())
+                {
+                    foreach (Cell c in r.Elements<Cell>())
+                    {
+                        text = c.CellValue.Text;
+                       // System.Diagnostics.Debug.WriteLine(text + " ");
+                    }
+                }
+            }
+
+        }
+
+
+        // The DOM approach.
+        // Note that the code below works only for cells that contain numeric values.
+        // 
+        static void ReadExcelFileDOM(string fileName)
+        {
+            using (SpreadsheetDocument spreadsheetDocument = SpreadsheetDocument.Open(fileName, false))
+            {
+                WorkbookPart workbookPart = spreadsheetDocument.WorkbookPart;
+                WorksheetPart worksheetPart = workbookPart.WorksheetParts.First();
+                SheetData sheetData = worksheetPart.Worksheet.Elements<SheetData>().First();
+                string text;
+                foreach (Row r in sheetData.Elements<Row>())
+                {
+                    foreach (Cell c in r.Elements<Cell>())
+                    {
+                        text = c.CellValue.Text;
+                        Console.Write(text + " ");
+                        System.Diagnostics.Debug.WriteLine(text + " ");
+                    }
+                }
+                //Console.WriteLine();
+                //Console.ReadKey();
+            }
+        }
+
+        // The SAX approach.
+        static void ReadExcelFileSAX(string fileName)
+        {
+            using (SpreadsheetDocument spreadsheetDocument = SpreadsheetDocument.Open(fileName, false))
+            {
+                WorkbookPart workbookPart = spreadsheetDocument.WorkbookPart;
+                WorksheetPart worksheetPart = workbookPart.WorksheetParts.First();
+
+                DocumentFormat.OpenXml.OpenXmlReader reader = DocumentFormat.OpenXml.OpenXmlReader.Create(worksheetPart);
+
+                string text;
+                while (reader.Read())
+                {
+                    if (reader.ElementType == typeof(CellValue))
+                    {
+                        text = reader.GetText();
+                        System.Diagnostics.Debug.WriteLine(text + " ");
+                    }
+                }
+                //Console.WriteLine();
+                //Console.ReadKey();
+            }
         }
 
     }
